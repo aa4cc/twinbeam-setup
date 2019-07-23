41d2584338890ebfc421d157f93c57674d3a9a89
Viktor-Adam Koropecký <koropvik@fel.cvut.cz>
2019-07-23 15:26:00 +0200

    finishing the settings separation

--- JetsonCode/Settings.cpp
+++ JetsonCode/Settings.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int Settings::values[] = {1024, 1024, 1195, 500, 5000000, 3100, 2750};
 
-void Settings::set_setting(int index, int new_setting)
+void Settings::set_setting(int index, const int new_setting){
 	Settings::values[index] = new_setting;
+}
