dda42f599b61d74f256553e1e56d2c662810d8be
Viktor-Adam Koropecký <koropvik@fel.cvut.cz>
2019-07-23 15:17:13 +0200

    finishing the settings separation

--- JetsonCode/Settings.cpp
+++ JetsonCode/Settings.cpp
@@ -2,13 +2,7 @@
 
 using namespace std;
 
-int Settings::values[STG_WIDTH] = 1024;
-int Settings::values[STG_HEIGHT] = 1024;
-int Settings::values[STG_OFFSET_X] = 1195;
-int Settings::values[STG_OFFSET_Y] = 500;
-int Settings::values[STG_EXPOSURE] = 5000000;
-int Settings::values[STG_Z_RED] = 3100;
-int Settings::values[STG_Z_GREEN] = 2750;
+int Settings::values[] = {1024, 1024, 1195, 500, 5000000, 3100, 2750};
 
 void Settings::set_setting(int index, int new_setting)
 	Settings::values[index] = new_setting;
