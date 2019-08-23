71bd545ddba314071c971734d955deb3f4954b32
Viktor-Adam Koropecký <koropvik@fel.cvut.cz>
2019-08-23 12:21:56 +0200

    Fixing exposure and gain in camera settings

--- JetsonCode/Trial.cu
+++ JetsonCode/Trial.cu
@@ -509,7 +509,7 @@ void consumer_thread(){
 			ISourceSettings *iSourceSettings = interface_cast<ISourceSettings>(iRequest->getSourceSettings());
 			iSourceSettings->setFrameDurationRange(Range<uint64_t>(1e9/DEFAULT_FPS));
 			iSourceSettings->setExposureTimeRange(Range<uint64_t>(Settings::values[STG_EXPOSURE],Settings::values[STG_EXPOSURE]));
-			//iSourceSettings->setGain
+			iSourceSettings->setGainRange(Range<float>(1.0,1.0));
 
 			cudaMalloc(&G, Settings::get_area()*sizeof(uint16_t));
 			cudaMalloc(&R, Settings::get_area()*sizeof(uint16_t));
