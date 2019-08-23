0fb4526b966bc82f031582e0c8f4071e6dcd1ad3
Viktor-Adam Koropecký <koropvik@fel.cvut.cz>
2019-08-23 13:10:52 +0200

    Fixing exposure and gain in camera settings

--- JetsonCode/Trial.cu
+++ JetsonCode/Trial.cu
@@ -510,8 +510,8 @@ void consumer_thread(){
 			iSourceSettings->setFrameDurationRange(Range<uint64_t>(1e9/DEFAULT_FPS));
 			iSourceSettings->setExposureTimeRange(Range<uint64_t>(Settings::values[STG_EXPOSURE],Settings::values[STG_EXPOSURE]));
 			iSourceSettings->setGainRange(Range<float>(1.0,1.0));
-			iSourceSettings->setOpticalBlackEnable(true);
-			iSourceSettings->setOpticalBlack(0.01);
+			//iSourceSettings->setOpticalBlackEnable(true);
+			//iSourceSettings->setOpticalBlack(0.01);
 
 			IAutoControlSettings *iAutoSettings = interface_cast<IAutoControlSettings>(iRequest->getAutoControlSettings());
 			iAutoSettings->setExposureCompensation(0);
