a81637dbde62ee2fda2674edea00be7b716fd75e
Viktor-Adam Koropecký <koropvik@fel.cvut.cz>
2019-09-03 14:11:44 +0200

    Still fixing camera flicker

--- JetsonCode/Trial.cu
+++ JetsonCode/Trial.cu
@@ -519,7 +519,7 @@ void consumer_thread(){
 			iAutoSettings->setColorSaturationBias(1.0);
 			iAutoSettings->setColorSaturationEnable(true);
 			iAutoSettings->setAwbLock(true);
-			iAutoSettings->setAeAntibandingMode(AE_ANTIBANDING_MODE_AUTO);
+			iAutoSettings->setAeAntibandingMode(AE_ANTIBANDING_MODE_OFF);
 
 			cudaMalloc(&G, Settings::get_area()*sizeof(uint16_t));
 			cudaMalloc(&R, Settings::get_area()*sizeof(uint16_t));
