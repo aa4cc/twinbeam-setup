68415a37af64d64f890a7b4cc38d85c1ee103115
Viktor-Adam Koropecký <koropvik@fel.cvut.cz>
2019-09-04 14:20:03 +0200

    Still fixing camera flicker

--- JetsonCode/Trial.cu
+++ JetsonCode/Trial.cu
@@ -526,8 +526,8 @@ void consumer_thread(){
 			iAutoSettings->setAwbLock(true);
 			iAutoSettings->setAeAntibandingMode(AE_ANTIBANDING_MODE_OFF);
 
-			IDenoiseSettings *iDenoiseSettings = interface_cast<iDenoiseSettings>(request);	
-			iDenoiseSettings->setDenoiseMode(FAST);
+			IDenoiseSettings *iDenoiseSettings = interface_cast<IDenoiseSettings>(request);	
+			iDenoiseSettings->setDenoiseMode(DENOISE_MODE_FAST);
 			iDenoiseSettings->setDenoiseStrength(1.0);
 
 			cudaMalloc(&G, Settings::get_area()*sizeof(uint16_t));
