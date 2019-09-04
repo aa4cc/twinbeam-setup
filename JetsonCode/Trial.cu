00cae79783ca4e1bd48de4ddc7570ebee054d8a2
Viktor-Adam Koropecký <koropvik@fel.cvut.cz>
2019-09-04 14:06:24 +0200

    Still fixing camera flicker

--- JetsonCode/Trial.cu
+++ JetsonCode/Trial.cu
@@ -809,7 +809,7 @@ void print_thread(){
 				cv::flip(img, img_trans, -1);
 				cv::transpose(img_trans, img);
 
-				const cv::Mat result = img2;
+				const cv::Mat result = img2*Settings::get_area()/256;
 				cv::imshow("Basic Visualization", result);
 				cv::waitKey(1);
 			}
