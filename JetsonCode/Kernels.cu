5d5b93f34581ed6e22fe5a946395bd2e31fc1727
Viktor-Adam Koropecký <koropvik@fel.cvut.cz>
2019-09-08 14:58:26 +0200

    Implementing a bluring filter

--- JetsonCode/Kernels.cu
+++ JetsonCode/Kernels.cu
@@ -422,7 +422,7 @@ __global__ void blurFilter(int M, int N, int margin, cufftComplex* input){
     int count = N*M;
     for(int i = index; i < count; i += stride){
         if((i % M) < margin || (i / M) < margin || (i / M) > N-margin || (i % M) > M-margin){
-            //input[i].x = 0;
+            input[i].y = 0;
             input[i].x = 0;
         }
     }
